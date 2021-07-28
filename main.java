3c7b675738905d50f116f6db28da4f1970832915
Saloni <85846998+Saloni0001@users.noreply.github.com>
2021-07-28 20:39:49 +0530

    created main.java
    This file includes how  packages and methods made in a java program.

---
+++ main.java
@@ -0,0 +1,14 @@
+package pacmethod;
+
+public class main {
+    // create a method
+    public int addNumbers( int a, int b){
+        int sum = a + b;
+        // return value
+        return sum;
+    }
+    public static void main(String[] args){
+        int num1 = 25;
+        int num2 = 15;
+    }
+}
